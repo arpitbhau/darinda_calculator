c6f3e947f1559b923221b377320367773cf6ea8b
arpitbhau <arpitnikhade20@gmail.com>
2025-04-30 20:37:02 +0530

    code optimization of the branch feature/functional-calc

--- script.js
+++ script.js
@@ -1,28 +1,5 @@
 // Jai Shree Ram
 
-// usage of taking outcome
-
-dhokebaaz.getOutcome()
-
-// store that thing in variable
-
-let outcome = dhokebaaz.getOutcome()
-
-// whenever you call that fucntion n your js you will get (i.e 2 false  value and 3 true values in your 5 consectives function calls) 
-
-// e.g
-
-for (let i = 0; i < 5 ;i++) {
-
-    console.log(dhokebaaz.getOutcome());
-    // we will get 2 false and 3 true every time you call (in backside it is taking 5 cases , just to know.)
-}
-
-
-// hope u complete this calculator with functionality and BEREHMII 
-
-// Arpit -_-
-
 let currentInput = "0";
 let previousInput = "0";
 let operation = null;
@@ -37,7 +14,6 @@ function updateDisplay() {
     display.textContent = currentInput;
     calculation.textContent = calculationString || "0";
 }
-
 
 function inputDigit(digit) {
     if (resetInput) {
@@ -151,30 +127,37 @@ function calculatePercentage() {
     updateDisplay();
 }
 
-// Event listeners for No. btns
-document.getElementById("zero").addEventListener("click", () => inputDigit("0"));
-document.getElementById("one").addEventListener("click", () => inputDigit("1"));
-document.getElementById("two").addEventListener("click", () => inputDigit("2"));
-document.getElementById("three").addEventListener("click", () => inputDigit("3"));
-document.getElementById("four").addEventListener("click", () => inputDigit("4"));
-document.getElementById("five").addEventListener("click", () => inputDigit("5"));
-document.getElementById("six").addEventListener("click", () => inputDigit("6"));
-document.getElementById("seven").addEventListener("click", () => inputDigit("7"));
-document.getElementById("eight").addEventListener("click", () => inputDigit("8"));
-document.getElementById("nine").addEventListener("click", () => inputDigit("9"));
+function select(identifier) {
+    return document.querySelector(identifier)
+}
 
-// Event listeners for operator btns
-document.getElementById("add").addEventListener("click", () => handleOperator("+"));
-document.getElementById("subtract").addEventListener("click", () => handleOperator("-"));
-document.getElementById("multiply").addEventListener("click", () => handleOperator("*"));
-document.getElementById("divide").addEventListener("click", () => handleOperator("/"));
-document.getElementById("equals").addEventListener("click", calculate);
+// event listener for all
 
+select(".calcBtns")
+.addEventListener("click" , e => {
+    
+    let target = e.target.id
 
-// Event listeners for other btns
-document.getElementById("clear").addEventListener("click", resetCalculator);
-document.getElementById("decimal").addEventListener("click", inputDecimal);
-document.getElementById("percent").addEventListener("click", calculatePercentage);
+    if ( !(isNaN(+target) )) inputDigit(target)
+
+    else if (isNaN(+target) && target.length === 1) handleOperator(target)
+
+    
+    switch (target) {
+
+        // TODO: make dhokebaaz functionality of calculator as a function and then call that function instead of that console.log()
+
+        case "equals": dhokebaaz.getOutcome() ? calculate() : console.log("Bhai galat calc vala function bana"); break
+        
+        case "percent": dhokebaaz.getOutcome() ? calculatePercentage() : console.log("Bhai galat calc vala function bana")
+
+        case "clear": resetCalculator(); break
+        case "decimal": inputDecimal(); break
+        
+    }
+
+})
+
 
 
 document.addEventListener("keydown", (event) => {
